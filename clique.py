605ddc5707c689e203e7baf90ec81ac6d6453e9d
alejontnu <alejontnu@gmail.com>
2021-07-06 12:59:28 +0200

    Some more work on clique algorithm

--- clique.py
+++ clique.py
@@ -4,11 +4,77 @@ import JSON_IO as json
 import aim_trajectory_picking.functions as func
 import matplotlib.pyplot as plt
 
+def translate_trajectory_objects_to_dictionaries(trajectories):
+    new_list = []
+    for item in trajectories:
+        new_list.append(item.__dict__)
+    return new_list
+
+# def translate_trajectory_objects_to_dictionaries(trajectories):
+#     return [e.__dict__ for e in trajectories]
+
+def make_transformed_graph_from_trajectory_dictionaries(trajectory_dictionaries):
+    G = nx.Graph()
+    ids = [e['id'] for e in trajectory_dictionaries]
+    G.add_nodes_from(ids)
+    node_attrs = {}
+    for i in range(len(trajectory_dictionaries)):
+        node_attrs[ids[i]] = trajectory_dictionaries[i]
+    nx.set_node_attributes(G,node_attrs)
+    for i in range(len(trajectory_dictionaries)):
+        for j in range(i, len(trajectory_dictionaries)):
+            if i != j:
+                if mutually_exclusive_trajectories_dictionary(trajectory_dictionaries[i], trajectory_dictionaries[j]):
+                    G.add_edge(trajectory_dictionaries[i]['id'], trajectory_dictionaries[j]['id'])
+    return G
+
+# def make_transformed_graph_from_trajectory_dictionaries(trajectory_dictionaries,values):
+#     G = nx.Graph()
+#     ids = [e['id'] for e in trajectory_dictionaries]
+#     G.add_nodes_from(ids)
+#     for i in range(len(trajectory_dictionaries)):
+#         nx.set_node_attributes(G,values, trajectory_dictionaries[i])
+#     for i in range(len(trajectory_dictionaries)):
+#         for j in range(i, len(trajectory_dictionaries)):
+#             if i != j:
+#                 if mutually_exclusive_trajectories_dictionary(trajectory_dictionaries[i], trajectory_dictionaries[j]):
+#                     G.add_edge(trajectory_dictionaries[i], trajectory_dictionaries[j])
+#     return G
+
+# def make_transformed_graph_from_trajectory_dictionaries(trajectory_dictionaries):
+#     G = nx.Graph()
+#     G.add_nodes_from(trajectory_dictionaries)
+#     for i in range(len(trajectory_dictionaries)):
+#         for j in range(i, len(trajectory_dictionaries)):
+#             if i != j:
+#                 if mutually_exclusive_trajectories_dictionary(trajectory_dictionaries[i], trajectory_dictionaries[j]):
+#                     G.add_edge(trajectory_dictionaries[i], trajectory_dictionaries[j])
+#     return G
+ 
+def mutually_exclusive_trajectories_dictionary(t1, t2):
+    if t1['donor'] == t2['donor']:
+        return True
+    elif t1['target'] == t2['target']:
+        return True
+    elif t1['id'] in t2['collisions']:
+        return True
+    return False
+
 directory = r'.\basesets'
 filename = 'base_test_0.txt'
 fullpath = os.path.join(directory,filename)
 list_of_trajectories = json.read_trajectory_from_json(fullpath)
-G = func.transform_graph(list_of_trajectories)
+
+values = []
+for i in range(len(list_of_trajectories)):
+    values.append(list_of_trajectories[i].value)
+
+trajectories_dict = translate_trajectory_objects_to_dictionaries(list_of_trajectories)
+print(trajectories_dict)
+print(trajectories_dict[1])
+G = make_transformed_graph_from_trajectory_dictionaries(trajectories_dict)
+
+
 
 visualize = True
 
@@ -26,4 +92,7 @@ def clique_set(G,weights=None,visualize=False):
         nx.draw(C,with_labels=True)
         plt.show()
 
-C, weights = clique_set(G,None,True)
+weights = 'value'
+C, weights = clique_set(G,weights,True)
+
+
