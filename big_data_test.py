6fa90c92869a31271bbd8d16283b022e3f17c665
alejontnu <alejontnu@gmail.com>
2021-07-08 13:01:40 +0200

    Fixed bad merge conflict merge

--- big_data_test.py
+++ big_data_test.py
@@ -2,20 +2,24 @@ from matplotlib.pyplot import plot
 import aim_trajectory_picking.functions as func
 from integration_test import plot_performances
 import JSON_IO
+import time
 
 if __name__ == '__main__':
-    test_functions = [func.greedy_algorithm, func.NN_algorithm,func.random_algorithm, func.weight_transformation_algorithm, func.bipartite_matching_removed_collisions, func.lonely_target_algorithm]
+    #test_functions = [func.greedy_algorithm, func.NN_algorithm,func.random_algorithm, func.weight_transformation_algorithm, func.bipartite_matching_removed_collisions, func.lonely_target_algorithm]
+    test_functions = [func.modified_greedy]
     combined_results = {}
     for algorithm in test_functions:
         combined_results[algorithm.__name__] = []
-    BIG_data = JSON_IO.read_trajectory_from_json('big_datasets/highD_highT_highT.txt')
-    print("done reading")
+    start = time.perf_counter()
+    BIG_data, collisions = JSON_IO.read_trajectory_from_json_v2('big_datasets/highD_highT_highT.txt')
+    stop = time.perf_counter()
+    print("done reading with time: " + str(start-stop))
 
 
     for algorithm in test_functions:
-            answer = algorithm(BIG_data, False)
+            answer = algorithm(BIG_data,collisions, False)
             combined_results[algorithm.__name__].append(answer)
-            print("done with algorithm: " + algorithm )
+            print("done with algorithm: " + algorithm.__name__ )
     
     JSON_IO.write_data_to_json_file('big_data_results.txt', combined_results)
     for i in range(5):
