dacbc33e1d0af8962338aa7084417c50bafcf59c
jona1208 <64361897+jona1208@users.noreply.github.com>
2021-07-12 10:42:29 +0200

    added sort

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -65,20 +65,20 @@ def test_greedy_on_datasets_0_to_4():
 
 def test_NN_on_datasets_0_to_4():
     targeted_result_list = algorithm_test_function(func.NN_algorithm)
-    assert targeted_result_list == [32, 24, 20, 32, 26]#[32, 20, 26, 32, 24]
+    assert targeted_result_list.sort() == [32, 24, 20, 32, 26].sort()#[32, 20, 26, 32, 24]
 
 def test_weight_on_datasets_0_to_4():
     targeted_result_list = algorithm_test_function(func.weight_transformation_algorithm)
-    assert targeted_result_list ==  [31, 24, 24, 29, 26]#[29, 24, 26, 31, 24]
+    assert targeted_result_list.sort() ==  [31, 24, 24, 29, 26].sort()#[29, 24, 26, 31, 24]
 
 def test_bipartite_removed_collision_on_datasets_0_to_4():
     targeted_result_list = algorithm_test_function(func.bipartite_matching_removed_collisions)
-    assert targeted_result_list == [32, 23, 24, 32, 26]#[32, 24, 26, 32, 23]
+    assert targeted_result_list.sort() == [32, 23, 24, 32, 26].sort()#[32, 24, 26, 32, 23]
 
 def test_lonely_target_on_datasets_0_to_4():
     targeted_result_list = algorithm_test_function(func.lonely_target_algorithm)
-    assert targeted_result_list == [32, 24, 11, 32, 23]#[32, 11, 23, 32, 24]
+    assert targeted_result_list.sort() == [32, 24, 11, 32, 23].sort()#[32, 11, 23, 32, 24]
 
 def test_reverse_greedy_on_datasets_0_to_4():
     targeted_result_list = algorithm_test_function(func.reversed_greedy)
-    assert targeted_result_list == [31, 23, 24, 29, 26]#[29, 24, 26, 31, 23]
+    assert targeted_result_list == [31, 23, 24, 29, 26].sort()#[29, 24, 26, 31, 23]
